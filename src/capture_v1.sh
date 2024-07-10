a4972c2b21665d7c019c527a210a801070082d63
databus3301 <felix@rose.ruhr>
2024-07-10 13:09:11 +0200

    improve capture capabilities \n -> segmenting

--- src/capture_v1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ffmpeg -f v4l2 -i /dev/video2 -f rawvideo -pix_fmt yuyv422 - | ffplay -f rawvideo -infbuf -framedrop -fs -pixel_format yuyv422 -video_size 640x480 - &
-sleep 4
-maim -u -f png -q -i `xdotool getactivewindow` > "$PWD"/sc.png &
---
+++ src/segment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf segments
+mkdir segments
+
+ffmpeg -i ./capture/output.mp4 ./segments/frame_%04d.png
