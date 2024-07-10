c7b032dd4aa364bc2a1f00f6f354ca25cd54c8b1
databus3301 <felix@rose.ruhr>
2024-07-10 20:01:30 +0200

    testing servo

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,24 +1,17 @@
-#include <Servo.h>
-
-Servo myServo;
 const int servoPin = 5;
 
 void setup() {
-  myServo.attach(servoPin);
-  //pinMode(servoPin, OUTPUT);
+    pinMode(servoPin, OUTPUT);
 }
 
 void loop() {
-  // Set pulse width directly (1000 to 2000 microseconds for 0 to 180 degrees)
-  myServo.writeMicroseconds(1500);  // Center position
-  delay(1000);  // Wait for 1 second
-
-  myServo.writeMicroseconds(1000);  // 0 degrees
-  delay(1000);  // Wait for 1 second
-
-  myServo.writeMicroseconds(2000);  // 180 degrees
-  delay(1000);  // Wait for 1 second
-
-  
-  myServo.read()
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(pwmPin, i);
+    delay(2000/255); 
+  }
+  delay(1000);
+  for (int i = 255; i >= 0; i--) {
+    analogWrite(pwmPin, i);
+    delay(2000/255); 
+  }
 }
