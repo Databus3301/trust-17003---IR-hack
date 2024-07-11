a1e63b81462d3b2c9f2c7c1ad4f998431bd0e9ab
databus3301 <felix@rose.ruhr>
2024-07-11 12:03:18 +0200

    bidirectional servo

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,17 +1,44 @@
-const int servoPin = 5;
+const int pwm1 = 5;
+const int pwm2 = 3;
 
 void setup() {
-    pinMode(servoPin, OUTPUT);
+    pinMode(pwm1, OUTPUT);
+    pinMode(pwm2, OUTPUT);
 }
 
 void loop() {
   for (int i = 0; i <= 255; i++) {
-    analogWrite(pwmPin, i);
-    delay(2000/255); 
+    analogWrite(pwm1, i);
+    digitalWrite(pwm2, LOW);
+
+    delay(4000/255);
   }
-  delay(1000);
+  delay(100);
+  
+
   for (int i = 255; i >= 0; i--) {
-    analogWrite(pwmPin, i);
-    delay(2000/255); 
+    analogWrite(pwm1, i);
+    digitalWrite(pwm2, LOW);
+
+    delay(4000/255);
+  }
+
+  
+  delay(100);
+
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(pwm2, i);
+    digitalWrite(pwm1, LOW);
+
+    delay(4000/255);
+  }
+
+  delay(100);
+  
+  for (int i = 255; i >= 0; i--) {
+    analogWrite(pwm2, i);
+    digitalWrite(pwm1, LOW);
+
+    delay(4000/255);
   }
 }
