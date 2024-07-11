a49fab71bfa2371251fee3edaea5ad1415505804
databus3301 <felix@rose.ruhr>
2024-07-11 15:53:13 +0200

    bash control of cam orientation

--- src/control.sh
+++ src/control.sh
@@ -4,9 +4,5 @@ SERIAL_PORT="/dev/ttyACM0"
 
 # Configure the serial port
 sudo chmod a+rw $SERIAL_PORT
-sleep 3
-sudo fuser -k $SERIAL_PORT
-sleep 3
-stty -F $SERIAL_PORT 9600 cs8 -cstopb -parenb
-sleep 3
-echo -n 'l' > $SERIAL_PORT
+
+echo -n $1 | minicom -o -D /dev/ttyACM0 -b 9600
