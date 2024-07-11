b50ff34ad91d0fabc11b55305829b918ce6c00b6
databus3301 <felix@rose.ruhr>
2024-07-11 15:43:23 +0200

    tried delays

--- src/control.sh
+++ src/control.sh
@@ -4,7 +4,9 @@ SERIAL_PORT="/dev/ttyACM0"
 
 # Configure the serial port
 sudo chmod a+rw $SERIAL_PORT
+sleep 3
 sudo fuser -k $SERIAL_PORT
+sleep 3
 stty -F $SERIAL_PORT 9600 cs8 -cstopb -parenb
-
-echo "l" > $SERIAL_PORT
+sleep 3
+echo -n 'l' > $SERIAL_PORT
