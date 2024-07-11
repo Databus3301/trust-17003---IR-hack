edbd521605fbd45f19ab6c0859b11805d300fbb9
databus3301 <felix@rose.ruhr>
2024-07-11 15:21:33 +0200

    debugging further

--- src/control.sh
+++ src/control.sh
@@ -3,6 +3,8 @@
 SERIAL_PORT="/dev/ttyACM0"
 
 # Configure the serial port
+sudo chmod a+rw $SERIAL_PORT
+sudo fuser -k $SERIAL_PORT
 stty -F $SERIAL_PORT 9600 cs8 -cstopb -parenb
 
 echo "l" > $SERIAL_PORT
--- src/sketch.ino
+++ src/sketch.ino
@@ -4,24 +4,45 @@ const int pwm2 = 3;
 void setup() {
     pinMode(pwm1, OUTPUT);
     pinMode(pwm2, OUTPUT);
+
+    Serial.begin(9600);
 }
 
 void left(int speed, int time) {
   digitalWrite(pwm2, LOW);
   analogWrite(pwm1, speed);
   delay(time);
+
+  digitalWrite(pwm2, LOW);
+  digitalWrite(pwm1, LOW);
 }
 
 void right(int speed, int time) {
   digitalWrite(pwm1, LOW);
   analogWrite(pwm2, speed);
   delay(time);
+
+  digitalWrite(pwm2, LOW);
+  digitalWrite(pwm1, LOW);
 }
 
 void loop() {
-    left(255, 500);
+    // read in input
+  if (Serial.available() > 0) {
+    char received = Serial.read(); // Read a character from the serial port
+    if(received == 'l') {
+      left(255, 500);
+    }
+    if(received == 'r') {
+      right(255, 500);
+    }
     delay(100);
-    right(255, 500);
-    delay(100);
+  }
+
+    //left(255, 500);
+    //delay(1000);
+    //right(255, 500);
+    //delay(5000);
 }
+
 
