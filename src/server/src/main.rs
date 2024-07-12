f74a143c6ef4e0ea7fd695f2173de0c75504f72a
databus3301 <felix@rose.ruhr>
2024-07-12 12:22:01 +0200

    better request handling

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,18 +1,17 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{BufReader, BufRead, Write, Read};
+use std::io::{Write, Read};
+use std::time::Duration;
 
 fn handle_client(mut stream: TcpStream) {
     println!("Client connected: {:?}", stream.peer_addr().unwrap());
 
-    let mut contents: String = Default::default();
-    {
-        let mut reader = BufReader::new(&stream);
-        reader.read_to_string(&mut contents).expect("Failed to read from stream");
-    }
+    let mut contents = [0; 8];
+    stream.read_exact(&mut contents).expect("Failed to read from stream");
+    let contents: String = contents.as_mut().iter().map(|&c| c as char).collect();
+    println!("Received:\n{}", contents);
 
-    for (i, l) in contents.lines().enumerate() {
+    for l in contents.lines().into_iter() {
         if l.starts_with("GET /l") {
-            println!("Received:\n{}", contents);
             std::process::Command::new("sh")
                 .arg("-c")
                 .arg("../control.sh l")
@@ -20,14 +19,16 @@ fn handle_client(mut stream: TcpStream) {
                 .expect("Failed to execute command");
         }
         if l.starts_with("GET /r") {
-            println!("Received: {}", contents);
             std::process::Command::new("sh")
                 .arg("-c")
                 .arg("../control.sh r")
                 .spawn()
                 .expect("Failed to execute command");
         }
-        stream.write(b"HTTP/1.1 200 OK\r\n").expect("Failed to write to stream");
+        stream.set_read_timeout(Option::from(Duration::new(3, 0))).expect("failed to set timeout");
+        if stream.read(&mut []).is_ok() {
+            stream.write(b"HTTP/1.1 200 OK\r\n").expect("Failed to write to stream");
+        }
     }
 }
 
@@ -38,10 +39,7 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                // Spawn a new thread for each incoming connection
-                std::thread::spawn(move || {
-                    handle_client(stream);
-                });
+                handle_client(stream);
             }
             Err(e) => {
                 eprintln!("Error accepting connection: {}", e);
