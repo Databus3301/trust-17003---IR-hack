9c195d908f321cc27b771f7f1bc6c3faf3c64b8b
databus3301 <felix@rose.ruhr>
2024-07-12 12:30:05 +0200

    fix connection reset

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -5,29 +5,39 @@ use std::time::Duration;
 fn handle_client(mut stream: TcpStream) {
     println!("Client connected: {:?}", stream.peer_addr().unwrap());
 
-    let mut contents = [0; 8];
-    stream.read_exact(&mut contents).expect("Failed to read from stream");
-    let contents: String = contents.as_mut().iter().map(|&c| c as char).collect();
-    println!("Received:\n{}", contents);
-
-    for l in contents.lines().into_iter() {
-        if l.starts_with("GET /l") {
-            std::process::Command::new("sh")
-                .arg("-c")
-                .arg("../control.sh l")
-                .spawn()
-                .expect("Failed to execute command");
-        }
-        if l.starts_with("GET /r") {
-            std::process::Command::new("sh")
-                .arg("-c")
-                .arg("../control.sh r")
-                .spawn()
-                .expect("Failed to execute command");
-        }
-        stream.set_read_timeout(Option::from(Duration::new(3, 0))).expect("failed to set timeout");
-        if stream.read(&mut []).is_ok() {
-            stream.write(b"HTTP/1.1 200 OK\r\n").expect("Failed to write to stream");
+    loop {
+        let mut contents = [0; 8];
+        match stream.read_exact(&mut contents) {
+            Ok(_) => {
+                let contents: String = contents.as_mut().iter().map(|&c| c as char).collect();
+                for l in contents.lines().into_iter() {
+                    if l.starts_with("GET /l") {
+                        std::process::Command::new("sh")
+                            .arg("-c")
+                            .arg("../control.sh l")
+                            .spawn()
+                            .expect("Failed to execute command");
+                    }
+                    if l.starts_with("GET /r") {
+                        std::process::Command::new("sh")
+                            .arg("-c")
+                            .arg("../control.sh r")
+                            .spawn()
+                            .expect("Failed to execute command");
+                    }
+                    stream.set_read_timeout(Option::from(Duration::new(3, 0))).expect("failed to set timeout");
+                    if stream.read(&mut []).is_ok() {
+                        match stream.write(b"HTTP/1.1 200 OK\r\n") {
+                            Ok(_) => (),
+                            Err(e) => eprintln!("Failed to write to stream: {}", e),
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Error reading from stream: {}", e);
+                break;
+            }
         }
     }
 }
