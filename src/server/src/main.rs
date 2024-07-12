ed765c295902fac15d58a45f6456780b6afe8384
databus3301 <felix@rose.ruhr>
2024-07-12 15:09:42 +0200

    image serving

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -14,6 +14,7 @@ fn handle_client(mut stream: TcpStream) {
                 for l in contents.lines().into_iter() {
                     if l.starts_with("GET / ") {
                         serve_html_file(&mut stream, "./src/index.html");
+                        serve_image_file(&mut stream, "./res/image.jpg");
                     }
                     if l.starts_with("GET /l") {
                         control("l");
@@ -69,4 +70,14 @@ fn serve_html_file(mut stream: &mut TcpStream, file_path: &str) {
     let contents: String = std::fs::read_to_string(file_path).expect("Failed to read file");
     let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{}", contents);
     serve_html(&mut stream, &response);
+}
+
+fn serve_image_file(mut stream: &mut TcpStream, file_path: &str) {
+    let contents: Vec<u8> = std::fs::read(file_path).expect("Failed to read file");
+    let response = "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\n\r\n";
+    serve_html(&mut stream, response);
+    match stream.write(&contents) {
+        Ok(_) => (),
+        Err(e) => eprintln!("Failed to write to stream: {}", e),
+    }
 }
