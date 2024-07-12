19e2c4d0deab4060593d0191f904b3185bcc05d5
databus3301 <felix@rose.ruhr>
2024-07-12 12:47:15 +0200

    more offloading + server html response

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,10 +1,10 @@
-use std::fmt::format;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Write, Read};
 use std::time::Duration;
 
 fn handle_client(mut stream: TcpStream) {
     println!("Client connected: {:?}", stream.peer_addr().unwrap());
+    stream.set_read_timeout(Option::from(Duration::new(3, 0))).expect("failed to set timeout");
 
     loop {
         let mut contents = [0; 8];
@@ -12,18 +12,16 @@ fn handle_client(mut stream: TcpStream) {
             Ok(_) => {
                 let contents: String = contents.as_mut().iter().map(|&c| c as char).collect();
                 for l in contents.lines().into_iter() {
+                    if l.starts_with("GET / ") {
+                        server_html(&mut stream, "./src/index.html");
+                    }
                     if l.starts_with("GET /l") {
                         control("l");
+                        serve_html(&mut stream, "HTTP/1.1 200 OK\r\n")
                     }
                     if l.starts_with("GET /r") {
                         control("r");
-                    }
-                    stream.set_read_timeout(Option::from(Duration::new(3, 0))).expect("failed to set timeout");
-                    if stream.read(&mut []).is_ok() {
-                        match stream.write(b"HTTP/1.1 200 OK\r\n") {
-                            Ok(_) => (),
-                            Err(e) => eprintln!("Failed to write to stream: {}", e),
-                        }
+                        serve_html(&mut stream, "HTTP/1.1 200 OK\r\n")
                     }
                 }
             }
@@ -57,4 +55,18 @@ fn control(sequence: &str) {
         .arg(format!("../control.sh {}", sequence))
         .spawn()
         .expect("Failed to execute command");
+}
+
+fn serve_html(mut stream: &mut TcpStream, html: &str) {
+    if stream.read(&mut []).is_ok() {
+        match stream.write(html.as_bytes()) {
+            Ok(_) => (),
+            Err(e) => eprintln!("Failed to write to stream: {}", e),
+        }
+    }
+}
+
+fn server_html(mut stream: &mut TcpStream, file_path: &str) {
+    let contents: String = std::fs::read_to_string(file_path).expect("Failed to read file");
+    serve_html(&mut stream, &contents);
 }
